bdc883aadd16e4a31fc40d78f4cfff004fdb90ba
Przemek <ppop91@gmail.com>
2019-02-17 11:24:08 +0100

    dołączenie aplikacji do projektu

--- sklep/settings.py
+++ sklep/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'products',
+    'products.apps.ProduktyConfig',
+    'Comments.apps.CommentsConfig',
+    'category.apps.CategoryConfig',
+    'Basket.apps.BasketConfig',
 ]
 
 MIDDLEWARE = [

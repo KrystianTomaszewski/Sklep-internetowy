8c905bfeeb8a0ff9d68e6458e513fe60be03b58e
witekosz <witekosz@gmail.com>
2019-02-17 10:52:20 +0100

    Stworzenie modelu Product

--- products/models.py
+++ products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+
+    c = [("1", "Option 1"), ("2", "Option 2")]
+
+    name = models.CharField(max_length=100)
+    price = models.FloatField
+    # size = models.ChoiceField(choices=c, label='Size')
+
+    def __str__(self):
+        return self.name
+
+
+
+
+

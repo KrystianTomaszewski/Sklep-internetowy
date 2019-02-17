f0e94df1c290ea99e34223986dd1046c2154d685
Krystian <krystian.tomaszewski3@gmail.com>
2019-02-17 12:04:54 +0100

    Kolejny commit

--- category/models.py
+++ category/models.py
@@ -5,7 +5,7 @@ from products.models import Product
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=10)
-    product = models.ManyToManyField(Product, null=True, blank=True)
+    product = models.ManyToManyField(Product, blank=True)
 
     def __str__(self):
         return self.name

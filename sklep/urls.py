2434db627ace09eae9e111530417164e38e1b07d
Krystian <krystian.tomaszewski3@gmail.com>
2019-02-17 14:44:52 +0100

    change urls

--- sklep/urls.py
+++ sklep/urls.py
@@ -20,4 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('Basket/', include('Basket.urls')),
     path('Comments/', include('Comments.urls')),
+    path('category/', include('category.urls')),
+
 ]
